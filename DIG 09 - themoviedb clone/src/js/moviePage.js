d76d3841de6e842a29e82819852d4f631eefa5a5
tamilnambi <123074613+tamilnambi@users.noreply.github.com>
2023-07-08 23:13:08 +0530

    search completed

--- DIG 09 - themoviedb clone/src/js/moviePage.js
+++ DIG 09 - themoviedb clone/src/js/moviePage.js
@@ -1,43 +1,48 @@
-
 getMovie();
 
-function getMovie(){
-    let id = localStorage.getItem('movieId');
+function getMovie() {
+  let id = localStorage.getItem("movieId");
 
-    const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDBhZjU4OTQzZjhiYzg2M2U1ZGM2ZWEyMDBkZWMzYSIsInN1YiI6IjY0OWIxNmQ2N2UzNDgzMDBhY2MzYTI2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TrFh8ToM1aVvdqNZHZK_KPoWHr_pYcy8OFWvmPqnt_s'
-        }
-      };
-      
-      fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US`, options)
-        .then(response => response.json())
-        .then(displayMovie)
-        .catch(err => console.error(err));
-    
-}
-function displayMovie(response){
-    console.log(response);
-    document.title = response.original_title + " - The Movie Database (TMDB)";
-    urlId = `https://image.tmdb.org/t/p/original${response.backdrop_path}`;
-    
-    getDominantColor(urlId);
-    
-}
-function getDominantColor(urlId){
-    const colorThief = new ColorThief();
-    const img = new Image();
-    var color;
-img.crossOrigin = 'Anonymous';
-let googleProxyURL = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=';
-img.src = googleProxyURL + encodeURIComponent(urlId);
-img.addEventListener('load', function() {
-   var color= colorThief.getColor(img);
-   console.log(color);
-   document.getElementById('main-section').style.backgroundImage= `url(${urlId})`;
-   document.getElementById('movie-details').style.backgroundColor = "rgba(" + color[0] + ", " + color[1] + ", " + color[2] + ", 0.75)";
-});
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDBhZjU4OTQzZjhiYzg2M2U1ZGM2ZWEyMDBkZWMzYSIsInN1YiI6IjY0OWIxNmQ2N2UzNDgzMDBhY2MzYTI2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TrFh8ToM1aVvdqNZHZK_KPoWHr_pYcy8OFWvmPqnt_s",
+    },
+  };
 
+  fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US`, options)
+    .then((response) => response.json())
+    .then(displayMovie)
+    .catch((err) => console.error(err));
+}
+function displayMovie(response) {
+  console.log(response);
+  document.title = response.original_title + " - The Movie Database (TMDB)";
+  backgroundUrlId = `https://image.tmdb.org/t/p/original${response.backdrop_path}`;
+  getBackground(backgroundUrlId);
+  posterUrlId = `https://image.tmdb.org/t/p/w500${response.poster_path}`;
+  console.log(posterUrlId);
+  document.getElementById(
+    "movie-poster"
+  ).innerHTML = `<img class="m-5" src="${posterUrlId}" height="500rem">`;
+}
+function getBackground(urlId) {
+  const colorThief = new ColorThief();
+  const img = new Image();
+  var color;
+  img.crossOrigin = "Anonymous";
+  let googleProxyURL =
+    "https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=";
+  img.src = googleProxyURL + encodeURIComponent(urlId);
+  img.addEventListener("load", function () {
+    var color = colorThief.getColor(img);
+    console.log(color);
+    document.getElementById(
+      "main-section"
+    ).style.backgroundImage = `url(${urlId})`;
+    document.getElementById("movie-details").style.backgroundColor =
+      "rgba(" + color[0] + ", " + color[1] + ", " + color[2] + ", 0.75)";
+  });
 }
--- DIG 09 - themoviedb clone/src/js/search.js
+++ DIG 09 - themoviedb clone/src/js/search.js
@@ -1,55 +1,90 @@
 let showSearchBar = false;
-function startSearchByDiv(){
-    let searchItem = document.getElementById('search-div-input').value;
-    localStorage.setItem('searchString',searchItem);
-    window.location.href = "search.html";
-}
-function toggleSearchBar(){
-    if(showSearchBar == false)
-    {
-        document.getElementById('search-field').style.display = "block";
-        document.getElementById('search-btn').innerHTML = `<i class="bi bi-x-lg text-light"></i>`;
-        showSearchBar = true;
-        getTrending();
-    }
-    else
-    {
-        document.getElementById('search-field').style.display = "none";
-        document.getElementById('search-btn').innerHTML = `<i class="bi bi-search text-primary"></i>`;
-        showSearchBar = false;
-    }
-}
-function getTrending(){
-    
-    const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDBhZjU4OTQzZjhiYzg2M2U1ZGM2ZWEyMDBkZWMzYSIsInN1YiI6IjY0OWIxNmQ2N2UzNDgzMDBhY2MzYTI2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TrFh8ToM1aVvdqNZHZK_KPoWHr_pYcy8OFWvmPqnt_s'
-        }
-      };
-      
-      fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
-        .then(response => response.json())
-        .then(showTrending)
-        .catch(err => console.error(err));
+document.getElementById("search").addEventListener("keyup", getDynamicSearch);
 
+function getDynamicSearch(event) {
+  let searchItem = document.getElementById("search").value;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDBhZjU4OTQzZjhiYzg2M2U1ZGM2ZWEyMDBkZWMzYSIsInN1YiI6IjY0OWIxNmQ2N2UzNDgzMDBhY2MzYTI2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TrFh8ToM1aVvdqNZHZK_KPoWHr_pYcy8OFWvmPqnt_s",
+    },
+  };
+
+  fetch(
+    `https://api.themoviedb.org/3/search/multi?query=${searchItem}&include_adult=false&language=en-US&page=1`,
+    options
+  )
+    .then((response) => response.json())
+    .then(displayDynamicItems)
+    .catch((err) => console.error(err));
+
+  if (event.key === "Enter") {
+    startSearchByBar(searchItem);
+  }
 }
-function showTrending(response){
-    let print = `<div class="d-flex py-2"><i class="bi bi-graph-up-arrow ms-3"></i><h6 class="ms-3 pt-1">Trending</h6></div>`;
+function displayDynamicItems(response){
+    let print="";
     let searchItems = [];
-    for(let i = 0; i<10;i++)
-    {
-        if(response.results[i].media_type == "tv")
-            searchItems.push(response.results[i].name);
-        else if(response.results[i].media_type == "movie")
-            searchItems.push(response.results[i].title);  
-        print +=   `<div class="d-flex py-1 search-suggestions"><i class="bi bi-search ms-3"></i><span class="ms-3" onclick="startSearchByBar('${searchItems[i]}')">${searchItems[i]}</span></div>`;
-    }
-    console.log(searchItems);
-    document.getElementById('suggestions').innerHTML = print;
+  for (let i = 0; i < 10; i++) {
+    if (response.results[i].media_type == "tv")
+      searchItems.push(response.results[i].name);
+    else if (response.results[i].media_type == "movie")
+      searchItems.push(response.results[i].title);
+    print += `<div class="d-flex py-1 search-suggestions"><i class="bi bi-search ms-3"></i><span class="ms-3" onclick="startSearchByBar('${searchItems[i]}')">${searchItems[i]}</span></div>`;
+  }
+  document.getElementById("suggestions").innerHTML = print;
 }
-function startSearchByBar(searchItem){
-    localStorage.setItem('searchString',searchItem);
-    window.location.href = "search.html";
+function startSearchByDiv() {
+  let searchItem = document.getElementById("search-div-input").value;
+  localStorage.setItem("searchString", searchItem);
+  window.location.href = "search.html";
+}
+function toggleSearchBar() {
+  if (showSearchBar == false) {
+    document.getElementById("search-field").style.display = "block";
+    document.getElementById(
+      "search-btn"
+    ).innerHTML = `<i class="bi bi-x-lg text-light"></i>`;
+    showSearchBar = true;
+    getTrending();
+  } else {
+    document.getElementById("search-field").style.display = "none";
+    document.getElementById(
+      "search-btn"
+    ).innerHTML = `<i class="bi bi-search text-primary"></i>`;
+    showSearchBar = false;
+  }
+}
+function getTrending() {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDBhZjU4OTQzZjhiYzg2M2U1ZGM2ZWEyMDBkZWMzYSIsInN1YiI6IjY0OWIxNmQ2N2UzNDgzMDBhY2MzYTI2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TrFh8ToM1aVvdqNZHZK_KPoWHr_pYcy8OFWvmPqnt_s",
+    },
+  };
+
+  fetch("https://api.themoviedb.org/3/trending/all/day?language=en-US", options)
+    .then((response) => response.json())
+    .then(showTrending)
+    .catch((err) => console.error(err));
+}
+function showTrending(response) {
+  let print = `<div class="d-flex py-2"><i class="bi bi-graph-up-arrow ms-3"></i><h6 class="ms-3 pt-1">Trending</h6></div>`;
+  let searchItems = [];
+  for (let i = 0; i < 10; i++) {
+    if (response.results[i].media_type == "tv")
+      searchItems.push(response.results[i].name);
+    else if (response.results[i].media_type == "movie")
+      searchItems.push(response.results[i].title);
+    print += `<div class="d-flex py-1 search-suggestions"><i class="bi bi-search ms-3"></i><span class="ms-3" onclick="startSearchByBar('${searchItems[i]}')">${searchItems[i]}</span></div>`;
+  }
+  document.getElementById("suggestions").innerHTML = print;
+}
+function startSearchByBar(searchItem) {
+  localStorage.setItem("searchString", searchItem);
+  window.location.href = "search.html";
 }
